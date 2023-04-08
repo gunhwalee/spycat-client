f807e35eb076476081c0c990b1201dd320dddc70
gunhwalee <ghlee2588@gmail.com>
2023-04-08 14:55:25 +0900

    feat: 로그인 동적구현

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Main() {
+  const name = useSelector(state => state.user.name);
+
+  console.log(name);
+
   return <div>Welcome~ This is Spy Cat</div>;
 }
 
